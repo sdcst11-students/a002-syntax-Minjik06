93fd61d058d548ddf658fadcbc595066bee0eced
Minjik06 <minjik525@deltalearns.ca>
2023-09-14 13:13:33 -0700

    vsdsd

--- maze.py
+++ maze.py
@@ -3,4 +3,3 @@ import turtle
 s = turtle.getscreen()
 s.bgpic('maze.gif')
 t = turtle.Turtle()
-
---
+++ sample.py
@@ -0,0 +1,11 @@
+# "help", "copyright", "credits" or "license" for more information.
+import turtle
+s = turtle.getscreen()
+t = turtle.Turtle()
+t.fd(200)
+t.forward(50)
+t.lt(30)
+t.forward(35)
+import turtle
+turtle.circle(100, 180) #Draws a circle with 180 degrees and 100 pixels radius(a half circle)
+turtle.left(90)
